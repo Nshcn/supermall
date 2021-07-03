d2d20b2095f798b3fe9c506667cd39c900ba063e
nshcn <2695563464@qq.com>
2021-07-03 21:54:07 +0800

    首页商品数据请求与结构设计

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -121,7 +121,7 @@ import NavBar from "components/common/navbar/NavBar";
 import TabControl from "components/content/tabcontrol/TabControl";
 
 
-import {getHomeMultidata} from "../../network/home";
+import {getHomeMultidata, getHomeGoods} from "../../network/home";
 
 export default {
   name: "Home",
@@ -137,14 +137,36 @@ export default {
     return {
       banner: [],
       recommend: [],
+      goods: {
+        'pop': {page: 0, list: []},
+        'new': {page: 0, list: []},
+        'sell': {page: 0, list: []}
+      }
     }
   },
   created() {
     // 1.请求多个数据
-    getHomeMultidata().then(res => {
-      this.banner = res.data.banner.list;
-      this.recommend = res.data.recommend.list;
-    })
+    this.getHomeMultidata();
+    // 2.请求商品数据
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banner = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+      })
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        console.log((res));
+        this.goods[type].list.push(...res.data.list)
+        this.goods[type].page += 1
+      })
+    }
   }
 }
 </script>
