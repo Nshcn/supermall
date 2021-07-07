0a7702a3d0db3828f235f9160fcfb28480015e51
nshcn <2695563464@qq.com>
2021-07-07 21:32:44 +0800

    记录离开Home时的状态

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -63,13 +63,24 @@ export default {
       currentType: 'pop',
       isShowBackTop: false,
       tabOffSetTop: 0,
-      isTabFixed: false
+      isTabFixed: false,
+      saveY: 0
     }
   },
   computed: {
     showGoods() {
       return this.goods[this.currentType].list
     }
+  },
+  destroyed() {
+    console.log('销毁');
+  },
+  activated() {
+    this.$refs.scroll.scrollTo(0, this.saveY, 0);
+    this.$refs.scroll.refresh();
+  },
+  deactivated() {
+    this.saveY = this.$refs.scroll.getScrollY();
   },
   created() {
     // 1.请求多个数据
