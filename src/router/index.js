e20bfca29238cf6539354f9d5168b86d99c9e7eb
nshcn <2695563464@qq.com>
2021-07-07 23:21:04 +0800

    实现首页跳转详情页携带iid

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-const Home=()=>import('../views/home/Home')
-const Profile=()=>import('../views/profile/Profile')
-const Cart=()=>import('../views/cart/Cart')
-const Category=()=>import('../views/category/Category')
+const Home = () => import('../views/home/Home')
+const Profile = () => import('../views/profile/Profile')
+const Cart = () => import('../views/cart/Cart')
+const Category = () => import('../views/category/Category')
+const Detail = () => import('../views/detail/Detail')
 
 Vue.use(VueRouter)
 
@@ -28,12 +29,16 @@ const routes = [
   {
     path: '/cart',
     component: Cart
+  },
+  {
+    path: '/detail',
+    component: Detail
   }
 ]
 
-const router=new VueRouter({
+const router = new VueRouter({
   routes,
-  mode:'history'
+  mode: 'history'
 })
 
 export default router
