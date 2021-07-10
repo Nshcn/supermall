45cdadd51cbef30365fde2552d3b15d125378116
nshcn <2695563464@qq.com>
2021-07-10 22:39:42 +0800

    详情页-商品评论数据展示

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -7,6 +7,7 @@
       <detail-shop-info :shop="shop"/>
       <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
       <detail-param-info :param-info="paramInfo"/>
+      <detail-comment-info :comment-info="commentInfo"/>
     </scroll>
   </div>
 </template>
@@ -21,10 +22,14 @@ import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
 import Scroll from "components/common/scroll/Scroll";
 import {getDetail, Goods, Shop, GoodsParam} from "network/detail";
 import DetailParamInfo from "./childComps/DetailParamInfo";
+import DetailCommentInfo from "./childComps/DetailCommentInfo";
 
 export default {
   name: "Detail",
-  components: {DetailParamInfo, DetailGoodsInfo, Scroll, DetailShopInfo, DetailBaseInfo, DetailSwiper, DetailNavBar},
+  components: {
+    DetailCommentInfo,
+    DetailParamInfo, DetailGoodsInfo, Scroll, DetailShopInfo, DetailBaseInfo, DetailSwiper, DetailNavBar
+  },
   data() {
     return {
       iid: null,
@@ -32,7 +37,8 @@ export default {
       goods: {},
       shop: {},
       detailInfo: {},
-      paramInfo: {}
+      paramInfo: {},
+      commentInfo: {}
     }
   },
   created() {
@@ -52,6 +58,10 @@ export default {
       this.detailInfo = data.detailInfo
       // 5.获取商品参数信息
       this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+      // 6. 获取评论信息
+      if (data.rate.cRate !== 0) {
+        this.commentInfo = data.rate.list[0]
+      }
     })
   },
   methods: {
